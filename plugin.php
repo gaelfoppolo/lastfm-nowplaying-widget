3e90c48ec049234d4141d6ab16c05aa9375cb967
Gaël Foppolo <me@gaelfoppolo.com>
2015-08-02 20:56:32 +0200

    some changes (see description)
    - heart (loved) next to song name
    - user info better display

--- plugin.php
+++ plugin.php
@@ -5,12 +5,12 @@
 	<title><?php echo $username; ?> on last.fm</title>
 	<link rel="stylesheet" type="text/css" href="styles/last.fm.css">
 	<style type="text/css">body { background: #<?php echo $bgcolor; ?> }</style>
-	<?php if($autorefresh) { ?><meta http-equiv="refresh" content="100"><?php } ?>
+	
 </head>
 <body>
 	<div id="lastfm" class="<?php echo $size; ?> center">
 		<div id="topbar" class="<?php echo $color; ?>">
-			<?php if($track['nowplaying']) { echo "now playing"; } else { echo "last played"; } ?> &middot; last.fm
+			<?php echo $playing = $track['nowplaying'] ? $nowplaying : $lastplayed.$track['date']; ?> sur Last.fm
 		</div>
 		<?php if(!empty($track['url'])) { ?><a target="_blank" href="<?php echo $track['url']; ?>"><?php } ?>
 
@@ -18,12 +18,14 @@
 		<?php if(!empty($track['url'])) { ?></a><?php } ?>
 
 		<div id="songinfo">
+			<song><?php echo $track['name']; ?> <span class="loved"><?php echo $track['userloved']; ?></span></song>
 			<artist><?php echo $track['artist']; ?></artist>
-			<song><?php echo $track['name']; ?></song>
 			<album><?php echo $track['album']; ?></album>
 		</div>
 		<div id="userinfo">
-			<?php echo $track['userloved']; if($track['userloved']) echo $delimiter; ?><strong>&#9835;:</strong> <?php echo $track['playcount'] . $delimiter; ?><strong>t:</strong> <?php echo $track['duration']. $delimiter; ?><strong>u:</strong> <a target="_blank" href="http://www.last.fm/user/<?php echo $username; ?>"><?php echo $username; ?></a>
+			<duration><?php echo $track['duration']; ?></duration>
+			<playcount><?php echo $track['playcount']; ?></playcount>
+			<user><a target="_blank" href="http://www.last.fm/user/<?php echo $username; ?>"><?php echo $username; ?></a></user>
 		</div>
 	</div>
 </body>
