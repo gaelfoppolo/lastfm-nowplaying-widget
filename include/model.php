2d169187291cb56685e5a7021604b74abd887b3a
Gaël Foppolo <me@gaelfoppolo.com>
2015-08-02 17:32:44 +0200

    new timezone + options in progress

--- include/model.php
+++ include/model.php
@@ -1,16 +1,17 @@
 <?php
 
-date_default_timezone_set('Europe/London');
+// Last.fm API UTC
+date_default_timezone_set('UTC');
 
-$api_key = "APIKEY";
+$api_key = "";
 
 // parse options
-$username = isset($_GET["username"]) ? $_GET["username"] : "icj_";
-$size = isset($_GET["size"]) ? $_GET["size"] : "medium";
-$bgcolor = isset($_GET["bgcolor"]) ? $_GET["bgcolor"] : "ffffff";
+$username = isset($_GET["username"]) ? $_GET["username"] : "iDrago";
+$size = isset($_GET["size"]) ? $_GET["size"] : "small";
 $autorefresh = isset($_GET["autorefresh"]) && ($_GET["autorefresh"] == "no") ? false : true;
 $color = isset($_GET["color"]) ? $_GET["color"] : "red";
-$delimiter = ($size == "tall") ? "<br>" : " &nbsp; "; 
+$nowplaying = 'En ce moment';
+$lastplayed = 'Il y a ';
 
 // headers
 header('X-Frame-Options: GOFORIT'); 
