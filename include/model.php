b24f5c1ccbff0ec9b8014486d1f0e2bb018ea067
Gaël Foppolo <me@gaelfoppolo.com>
2015-08-02 23:09:38 +0200

    removing some useless var

--- include/model.php
+++ include/model.php
@@ -1,29 +1,33 @@
 <?php
 
+include __DIR__ . "/LastFM_NowPlaying.php";
+
 // Last.fm API UTC
 date_default_timezone_set('UTC');
-
-$api_key = "";
-
-// parse options
-$username = isset($_GET["username"]) ? $_GET["username"] : "iDrago";
-$size = isset($_GET["size"]) ? $_GET["size"] : "big";
-$autorefresh = isset($_GET["autorefresh"]) && ($_GET["autorefresh"] == "no") ? false : true;
-$color = isset($_GET["color"]) ? $_GET["color"] : "red";
-$nowplaying = '<img src="images/np.gif"> En ce moment';
-$lastplayed = 'Il y a ';
 
 // headers
 header('X-Frame-Options: GOFORIT'); 
 header('Content-Type: text/html; charset=utf-8');
 
-include __DIR__ . "/class.lastfm-nowplaying.php";
+// options
+
+$api_key = "";
+
+$username = isset($_GET["username"]) ? $_GET["username"] : "iDrago";
+$size = isset($_GET["size"]) ? $_GET["size"] : "big";
+$color = isset($_GET["color"]) ? $_GET["color"] : "red";
 
 try {
-	$np = new lastfm_nowplaying($api_key, $size);
-	$track = $np->info($username);
+
+	$nowplaying = new LastFM_NowPlaying($size,$color,$username,$api_key);
+	$track = $nowplaying->info();
+	//header('Content-Type: text/plain');
+	//echo json_encode($track, JSON_PRETTY_PRINT);
+
 } catch (exception $e) {
-	printf("error %s", $e);
+	printf("%s", $e);
 }
+
+
 
 ?>
