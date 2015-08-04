6d0d1a172fcfeb35f8d19371325fe58ef61392d5
Gaël Foppolo <me@gaelfoppolo.com>
2015-08-04 21:56:46 +0200

    moving var to js

--- nowplaying.php
+++ nowplaying.php
@@ -7,14 +7,16 @@ date_default_timezone_set('UTC');
 
 // options to complete
 
+function getOptions(){
+
+}
+
 $api_key = "c3a41cdadfe269b2082b12eac19ec77f";
 $username = "iDrago";
-$size = "big";
-$color = "red";
 
 try {
 
-	$nowplaying = new LastFM_NowPlaying($size,$color,$username,$api_key);
+	$nowplaying = new LastFM_NowPlaying($username,$api_key);
 	$track = $nowplaying->info();
 	header('Content-Type: text/plain; charset=utf-8');
 	echo json_encode($track, JSON_PRETTY_PRINT);
