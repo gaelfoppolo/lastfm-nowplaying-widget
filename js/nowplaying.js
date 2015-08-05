36d557decb892b33c1b770698d885b01a712f2c9
Gaël Foppolo <me@gaelfoppolo.com>
2015-08-05 15:07:42 +0200

    DOM is now create, easier to implement in a website

--- js/nowplaying.js
+++ js/nowplaying.js
@@ -1,11 +1,89 @@
 window.addEventListener("load", function(){
 
 	var color = "red";
-	var size = "big";
+	var size = "small";
 
-	requestData(updateNowPlaying);
+	function createDOM() {
 
-	setInterval(requestData,60000,updateNowPlaying);
+		var lastfm = document.querySelector("#lastfm");
+		lastfm.setAttribute("class", size);
+
+		var topbar = document.createElement("div");
+		topbar.setAttribute("id", "topbar");
+		topbar.setAttribute("class", color);
+
+		lastfm.appendChild(topbar);
+
+		var img_np = document.createElement("img");
+		img_np.setAttribute("id","nowplaying")
+		img_np.setAttribute("src","images/np.gif");
+
+		var titlebar = document.createElement("titlebar");
+		titlebar.innerHTML = "Chargement...";
+
+		topbar.appendChild(img_np);
+		topbar.appendChild(titlebar);
+
+		var img_artwork = document.createElement("img");
+		img_artwork.setAttribute("id","artwork");
+		img_artwork.setAttribute("src","images/no_artwork.png");
+
+		lastfm.appendChild(img_artwork);
+
+		var songinfo = document.createElement("div");
+		songinfo.setAttribute("id","songinfo");
+
+		lastfm.appendChild(songinfo);
+
+		var song = document.createElement("song");
+
+		songinfo.appendChild(song);
+
+		var songname = document.createElement("name");
+
+		var loved = document.createElement("loved");
+
+		song.appendChild(songname);
+		song.appendChild(loved);
+
+		var artist = document.createElement("artist");
+
+		songinfo.appendChild(artist);
+
+		var album = document.createElement("album");
+
+		songinfo.appendChild(album);
+
+		var userinfo = document.createElement("div");
+		userinfo.setAttribute("id","userinfo");
+
+		lastfm.appendChild(userinfo);
+
+		var duration = document.createElement("duration");
+
+		userinfo.appendChild(duration);
+
+		var note = document.createElement("note");
+		var time = document.createElement("time");
+
+		duration.appendChild(note);
+		duration.appendChild(time);
+
+		var playcount = document.createElement("playcount");
+
+		userinfo.appendChild(playcount);
+
+		var user = document.createElement("user");
+
+		userinfo.appendChild(user);
+
+		var link_user = document.createElement("a");
+		link_user.setAttribute("target","_blank");
+		link_user.setAttribute("href","");
+
+		user.appendChild(link_user);
+
+	}
 
 	function requestData(callBack){
 
@@ -36,8 +114,10 @@ window.addEventListener("load", function(){
 		var album = document.querySelector("album");
 		album.innerHTML = is_too_long(data.album,23);
 		//duration
-		var duration = document.querySelector("time");
-		duration.innerHTML = data.duration;
+		var time = document.querySelector("time");
+		time.innerHTML = data.duration;
+		var note = document.querySelector("note");
+		note.innerHTML = "&#9835;";
 		//playcount
 		var playcount = document.querySelector("playcount");
 		playcount.innerHTML = data.playcount;		
@@ -69,5 +149,11 @@ window.addEventListener("load", function(){
 		return marquee.outerHTML;
 
 	}
+
+	createDOM();
+
+	requestData(updateNowPlaying);
+
+	setInterval(requestData,60000,updateNowPlaying);
 
 });
